74822e0f7cd5f8012df5c7009dd6a53db13f37a0
JC Ong <proxybetazero@gmail.com>
2021-06-25 12:14:13 +0800

    fixed spaces

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
     validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 } 
     validates :symbol, presence: true
+
+    has_many :transactions
 end
