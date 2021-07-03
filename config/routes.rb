8b2706fcceadeb5f79c015cf9852aa61d97fb450
desoto13 <mrmisa_windel@yahoo.com>
2021-07-03 10:10:17 +0800

    Final stock app with letter opener

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" 
+  # if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
