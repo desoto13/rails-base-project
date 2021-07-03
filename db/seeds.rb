bc76e94ac992405a51977b51aa563fff0dc4465a
desoto13 <mrmisa_windel@yahoo.com>
2021-07-03 10:51:22 +0800

    Config to Production

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ admin = Admin.create(email: "admin@email.com", password: "qwerty", type: "Admin"
 # buyer = Buyer.create(email: "jc2@email.com", password: "123123", type: "Buyer", username: "jc2" , approved: true)
 
 client = IEX::Api::Client.new(
-    config.publishable_token = Rails.application.credentials.IEX['IEX_API_PUBLISHABLE_TOKEN'] # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
-    config.secret_token = Rails.application.credentials.IEX['IEX_API_SECRET_TOKEN'] # defaults to ENV['IEX_API_SECRET_TOKEN']
-    config.endpoint = Rails.application.credentials.IEX['ENDPOINT'] # use 'https://sandbox.iexapis.com/v1' for Sandbox
+    publishable_token = Rails.application.credentials.IEX['IEX_API_PUBLISHABLE_TOKEN'] # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
+    secret_token = Rails.application.credentials.IEX['IEX_API_SECRET_TOKEN'] # defaults to ENV['IEX_API_SECRET_TOKEN']
+    endpoint = Rails.application.credentials.IEX['ENDPOINT'] # use 'https://sandbox.iexapis.com/v1' for Sandbox
 )
 
 symbols = client.ref_data_symbols()
