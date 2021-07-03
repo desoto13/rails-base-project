d2b9c6907692831a561beacfc031093c4e16c1ac
desoto13 <mrmisa_windel@yahoo.com>
2021-07-03 12:24:18 +0800

    For production new

--- config/initializers/iex_client.rb
+++ config/initializers/iex_client.rb
@@ -1,7 +1,7 @@
 IEX::Api.configure do |config|
-    config.publishable_token = Rails.application.credentials.IEX[:IEX_API_PUBLISHABLE_TOKEN] # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
-    config.secret_token = Rails.application.credentials.IEX[:IEX_API_PUBLISHABLE_TOKEN] # defaults to ENV['IEX_API_SECRET_TOKEN']
-    config.endpoint = Rails.application.credentials.IEX[:IEX_API_PUBLISHABLE_TOKEN] # use 'https://sandbox.iexapis.com/v1' for Sandbox
+    config.publishable_token = Rails.application.credentials.iex[:iex_api_publishable_token] # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
+    config.secret_token = Rails.application.credentials.iex[:iex_api_secret_token] # defaults to ENV['IEX_API_SECRET_TOKEN']
+    config.endpoint = Rails.application.credentials.iex[:endpoint] # use 'https://sandbox.iexapis.com/v1' for Sandbox
 end
 
 # IEX::Api.configure do |config|
